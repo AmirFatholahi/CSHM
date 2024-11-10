449f5a2fd5a8b4a690ee32b4e34227c88b2c0d80
am-fathollahi <am-fathollahi@agri-bank.ir>
2024-11-10 16:46:39 +0330

    feat: Complete ProductType Model to Controller #14030820-3

---
+++ CSHM.Presentation/Publish/PublishTypeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CSHM.Presentation.Publish;
+
+public class PublishTypeViewModel
+{
+    public int ID { get; set; }
+
+    public string Title { get; set; }
+
+    public bool IsActive { get; set; }
+}
