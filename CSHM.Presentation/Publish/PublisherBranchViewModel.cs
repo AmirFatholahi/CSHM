fccc624f102f83f146a02e36dce9e25d15165604
am-fathollahi <am-fathollahi@agri-bank.ir>
2024-11-12 18:11:22 +0330

    fix

---
+++ CSHM.Presentation/Publish/PublisherBranchViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace CSHM.Presentation.Publish;
+
+public class PublisherBranchViewModel
+{
+    public int ID { get; set; }
+
+    public int PublisherID { get; set; }
+
+    public string PublisherTitle { get; set; }
+
+    public int GeoCityID { get; set; }
+
+    public string GeoCityName { get; set; }
+
+    public string GeoCityCode { get; set; }
+
+    public string Title { get; set; }
+
+    public string Phone { get; set; }
+
+    public string Cellphone { get; set; }
+
+    public string Address { get; set; }
+
+    public bool IsActive { get; set; }
+}
---
+++ CSHM.Presentation/Publish/PublisherViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace CSHM.Presentation.Publish;
+
+public class PublisherViewModel
+{
+    public int ID { get; set; }
+
+    public string Title { get; set; }
+
+    public string Phone { get; set; }
+
+    public string Address { get; set; }
+
+    public string Cellphone { get; set; }
+
+    public string Website { get; set; }
+
+    public string Email { get; set; }
+
+    public bool IsActive { get; set; }
+}
